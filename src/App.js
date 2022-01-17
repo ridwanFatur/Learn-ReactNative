2f0fec8928dada5587df43e1bf94436b36d92736
LAPTOP-7LOBBHA8\Ridwan Faturrahman <ridwan.faturrahman.godjali@gmail.com>
2022-01-17 20:34:44 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import React from 'react';
-import {View} from 'react-native';
+import { View } from 'react-native';
+import { createStackNavigator } from '@react-navigation/stack';
+import MainScreen from './screens/MainScreen';
+import { NavigationContainer } from '@react-navigation/native';
 
-const App = () =>{
-	return <View></View>;
+const Stack = createStackNavigator();
+
+const App = () => {
+	return (
+		<NavigationContainer>
+			<Stack.Navigator initialRouteName='Main'>
+				<Stack.Screen name="Main" component={MainScreen} />
+			</Stack.Navigator></NavigationContainer>
+	);
 }
 
 export default App;
