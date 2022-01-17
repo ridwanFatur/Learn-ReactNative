067dae49ee83d1f3b7eb47c956de5e7a5049c7d2
LAPTOP-7LOBBHA8\Ridwan Faturrahman <ridwan.faturrahman.godjali@gmail.com>
2022-01-17 21:54:16 +0700

    update

--- src/screens/MainScreen.js
+++ src/screens/MainScreen.js
@@ -1,9 +1,30 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import { View, Text, Button } from 'react-native';
 
 const MainScreen = () => {
 	return <View>
 		<Text>Main Screen</Text>
+		<Button
+			title='Download'
+			onPress={() => {
+				console.log("Testing");
+				var RNFS = require('react-native-fs');
+				const ret = RNFS.downloadFile({
+					fromUrl: "https://i.ytimg.com/vi/X8sIaD7rKFQ/maxresdefault.jpg",
+					toFile: "Ridwan",
+					background: true,
+				});
+
+				ret.promise.then((res) => {
+					console.log('Download finished.');
+					// RNFS.completeHandlerIOS(jobId);
+					// jobId = -1;
+				}).catch(err => {
+					console.log('error');
+					// jobId = -1;
+				});
+			}}
+		/>
 	</View>;
 }
 
