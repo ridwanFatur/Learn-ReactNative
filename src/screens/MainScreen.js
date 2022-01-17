28c9c3d62b7d487343d462b87030a11fc554ec1a
LAPTOP-7LOBBHA8\Ridwan Faturrahman <ridwan.faturrahman.godjali@gmail.com>
2022-01-17 20:41:55 +0700

    update

--- src/screens/MainScreen.js
+++ src/screens/MainScreen.js
@@ -1,10 +1,34 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, { useRef } from 'react';
+import { View, Text, TextInput, Keyboard, TouchableWithoutFeedback } from 'react-native';
 
 const MainScreen = () => {
-	return <View>
-		<Text>Main Screen</Text>
-	</View>;
+	const ref_input2 = useRef();
+	const ref_input3 = useRef();
+
+	return <>
+		<TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+			<View style={{ flex: 1, padding: 20 }}>
+				<View style={{ height: 20 }} />
+				<TextInput
+					placeholder="Input1"
+					autoFocus={true}
+					returnKeyType="next"
+					onSubmitEditing={() => ref_input2.current.focus()}
+				/>
+				<TextInput
+					placeholder="Input2"
+					returnKeyType="next"
+					onSubmitEditing={() => ref_input3.current.focus()}
+					ref={ref_input2}
+				/>
+				<TextInput
+					placeholder="Input3"
+					ref={ref_input3}
+				/>
+			</View>
+		</TouchableWithoutFeedback>
+
+	</>;
 }
 
 export default MainScreen;
