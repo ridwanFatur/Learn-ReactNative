b2f363e01c65a369e859812d1b023ca02aa41a7a
LAPTOP-7LOBBHA8\Ridwan Faturrahman <ridwan.faturrahman.godjali@gmail.com>
2022-01-17 21:28:31 +0700

    update

--- src/screens/MainScreen.js
+++ src/screens/MainScreen.js
@@ -1,9 +1,16 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import { View, Text } from 'react-native';
+import SignatureCapture from 'react-native-signature-capture';
 
 const MainScreen = () => {
-	return <View>
-		<Text>Main Screen</Text>
+	function _onSaveEvent(result) {
+		//result.encoded - for the base64 encoded png
+		//result.pathName - for the file path name
+		console.log(result);
+	}
+
+	return <View style={{ flex: 1 }}>
+		<SignatureCapture style={{ flex: 1 }} onSaveEvent={_onSaveEvent} />
 	</View>;
 }
 
