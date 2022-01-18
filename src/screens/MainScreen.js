628d851d464c29be807441b7ca9ac2a2ca7904dc
LAPTOP-7LOBBHA8\Ridwan Faturrahman <ridwan.faturrahman.godjali@gmail.com>
2022-01-18 09:20:19 +0700

    update

--- src/screens/MainScreen.js
+++ src/screens/MainScreen.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { View, Text, Button } from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
 const MainScreen = () => {
 	return <View>
@@ -7,24 +8,33 @@ const MainScreen = () => {
 		<Button
 			title='Download'
 			onPress={() => {
-				console.log("Testing");
-				var RNFS = require('react-native-fs');
-				const ret = RNFS.downloadFile({
-					fromUrl: "https://i.ytimg.com/vi/X8sIaD7rKFQ/maxresdefault.jpg",
-					toFile: "Ridwan",
-					background: true,
-				});
+				// var RNFS = require('react-native-fs');
+				// var path = RNFS.DocumentDirectoryPath + '/image.jpg';
+				// console.log(path);
 
-				ret.promise.then((res) => {
-					console.log('Download finished.');
-					// RNFS.completeHandlerIOS(jobId);
-					// jobId = -1;
-				}).catch(err => {
-					console.log('error');
-					// jobId = -1;
-				});
+				console.log("Testing");
 			}}
 		/>
+		<TouchableOpacity onPress={() => {
+			var RNFS = require('react-native-fs');
+			var path = RNFS.ExternalStorageDirectoryPath + '/image.jpg';
+			const ret = RNFS.downloadFile({
+				fromUrl: "https://i.ytimg.com/vi/X8sIaD7rKFQ/maxresdefault.jpg",
+				toFile: path,
+				background: true,
+			});
+
+			ret.promise.then((res) => {
+				console.log('Download finished.');
+				// RNFS.completeHandlerIOS(jobId);
+			}).catch(err => {
+				console.log(err);
+			});
+		}}>
+			<View style={{ marginTop: 20, padding: 10, backgroundColor: 'blue', marginHorizontal: 10, borderRadius: 20, alignItems: 'center' }}>
+				<Text style={{ color: 'white' }}>Test</Text>
+			</View>
+		</TouchableOpacity>
 	</View>;
 }
 
